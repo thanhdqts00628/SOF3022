81160bc73e1721a36d3a68269b6ac098db76cfb8
Dương Quang Thanh <thanhdqts00628@fpt.edu.vn>
2025-10-19 07:52:38 +0700

    cleaned branch

--- src/main/java/poly/edu/dao/lab6/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package poly.edu.dao.lab6;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import poly.edu.model.lab6.Category;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, String> {
-}
--- src/main/java/poly/edu/dao/lab6/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package poly.edu.dao.lab6;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import poly.edu.model.lab6.Product;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
